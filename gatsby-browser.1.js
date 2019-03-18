7d7aa49c2c96fec0f3e8a894e6d6341b5a6216f2
Pedr <pedr@sleepstation.org.uk>
2019-03-18 10:37:16 +0000

    fix: Duplicate file

--- gatsby-browser.1.js
+++
@@ -1,6 +0,0 @@
-import '@babel/polyfill'
-
-// eslint-disable-next-line import/prefer-default-export
-export const onClientEntry = () => {
-  // Without this function body the import will not be picked up.
-}
